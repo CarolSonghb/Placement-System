d8122d83e940310fa3d7b69083ded7a0f4e93556
CarolSonghb <117868496+CarolSonghb@users.noreply.github.com>
2023-06-03 18:29:40 +1200

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,7 +1,7 @@
 
 
 dbuser = "root"  # PUT YOUR MySQL username here - usually admin
-dbpass = "Mushroomhead2021!"  # "PUT YOUR PASSWORD HERE"
+dbpass = ""  # "PUT YOUR PASSWORD HERE"
 dbhost = "localhost"  # "PUT YOUR AWS Connect String here"
 dbport = "3306"
 dbname = "ipps"
